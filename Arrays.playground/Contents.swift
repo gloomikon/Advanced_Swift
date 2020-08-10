0e694330ccbaead16248966dcadb293919afdd08
Nikolay Zhurba <gloomikon@gmail.com>
2020-08-10 22:03:54 +0300

    AdvancedSwift: Arrays finished

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -77,3 +77,66 @@ extension Array {
 
 let filtered = [1, 2, 3, 4, 5, 6].kfilter { $0 % 2 == 0}
 print(filtered) // [2, 4, 6]
+
+extension Sequence {
+    public func all(matching predicate: (Element) -> Bool) -> Bool {
+        return !contains { !predicate($0) }
+    }
+}
+
+let evenNumbers = [2, 4, 6, 8, 10]
+print(evenNumbers.all { $0 % 2 == 0}) // true
+
+extension Array {
+    func kreduce<Result>(_ initialResult: Result, _ nextParticialResult: (Result, Element) -> Result) -> Result {
+        var result = initialResult
+        for x in self {
+            result = nextParticialResult(result, x)
+        }
+        return result
+    }
+}
+
+let sumOfInts = [1, 2, 3, 4, 5].kreduce(0, +)
+print(sumOfInts) // 15
+
+extension Array {
+    // map function using only reduce
+    func mapWithReduce<T>(_ transform: (Element) -> T) -> [T] {
+        return reduce([]) { $0 + [transform($1)] }
+    }
+
+    // filter function using only reduce
+    func filterWithReduce(_ condition: (Element) -> Bool) -> [Element] {
+        return reduce([]) { condition($1) ? $0 + [$1] : $0 }
+    }
+
+    // filter with reduce with inout parametr
+    func filterReduceInout(_ condition: (Element) -> Bool) -> [Element] {
+        return reduce(into: []) { result, element in
+            if condition(element) {
+                result.append(element)
+            }
+        }
+    }
+}
+
+extension Array {
+    func kflatMap<T>(_ transform: (Element) -> [T]) -> [T] {
+        var result: [T] = []
+        for x in self {
+            result.append(contentsOf: transform(x))
+        }
+        return result
+    }
+}
+
+let suits = ["♠︎", "♥︎", "♣︎", "♦︎"]
+let ranks = ["J","Q","K","A"]
+let result = suits.flatMap { suit in
+    ranks.map { rank in
+        (suit, rank)
+    }
+}
+
+print(result)
