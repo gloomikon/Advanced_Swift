aedd1f99616a6605b0da729cbe59083e6ede8b1c
Nikolay Zhurba <gloomikon@gmail.com>
2020-08-24 21:48:34 +0300

    AdvancedSwift: Collections

---
+++ Collection Protocols/Collections.playground/Contents.swift
@@ -0,0 +1,95 @@
+import Foundation
+
+/// A type that can `enqueue` and `dequeue` elements
+
+protocol Queue {
+    /// A type of element held is `self`
+    associatedtype Element
+    /// Enqueue `element` to `self`
+    mutating func enqueue(_ element: Element)
+    /// Dequeue an element from `self`
+    mutating func dequeue() -> Element?
+}
+
+struct FIFOQueue<Element>: Queue {
+    private var left: [Element] = []
+    private var right: [Element] = []
+
+    /// Add an element to the back of the queue
+    /// - Complexity: O(1)
+    mutating func enqueue(_ element: Element) {
+        right.append(element)
+    }
+
+    /// Removes front of the queue
+    /// Returns `nil` in case of the empty queue
+    /// - Complexity: Amortized O(1)
+    mutating func dequeue() -> Element? {
+        if left.isEmpty {
+            left = right.reversed()
+            right.removeAll()
+        }
+        return left.popLast()
+    }
+}
+
+// MARK: - Conforming to Collection
+
+extension FIFOQueue: Collection {
+    public var startIndex: Int {
+        return 0
+    }
+
+    public var endIndex: Int {
+        return left.count + right.count
+    }
+
+    public func index(after i: Int) -> Int {
+        precondition(i < endIndex)
+        return i + 1
+    }
+
+    public subscript(position: Int) -> Element {
+        precondition((0..<endIndex).contains(position), "Index out of bounds")
+        if position < left.endIndex {
+            return left[left.count - position - 1]
+        }
+        else {
+            return right[position - left.count]
+        }
+    }
+}
+
+var q = FIFOQueue<String>()
+for x in ["1", "2", "foo", "3"] {
+    q.enqueue(x)
+}
+for s in q {
+    print(s, terminator: " ")
+} // 1 2 foo 3
+print()
+
+var a = Array(q) // ["1", "2", "foo", "3"]
+a.append(contentsOf: q[2...3])
+print(a) // ["1", "2", "foo", "3", "foo", "3"]
+
+// MARK: - Conforming to ExpressibleByArrayLiteral
+
+extension FIFOQueue: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self.left = elements.reversed()
+        self.right = []
+    }
+}
+
+let queue: FIFOQueue = [1, 2, 3] // FIFOQueue<Int>(left: [3, 2, 1], right: [])”
+
+let byteQueue: FIFOQueue<UInt8> = [1, 2, 3] // FIFOQueue<UInt8>(left: [3, 2, 1], right: [])
+
+extension FIFOQueue {
+    typealias Indices = CountableRange<Int>
+
+    var indices: CountableRange<Int> {
+        return startIndex..<endIndex
+    }
+}
